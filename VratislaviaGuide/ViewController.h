d63278074deb057a011cac915a434e54c1506aee
Waldemar Walczak <waldemar.walczak@smtsoftware.com>
2015-03-22 18:34:20 +0100

    Initial Commit

---
+++ VratislaviaGuide/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VratislaviaGuide
+//
+//  Created by Waldemar Walczak on 22/03/15.
+//  Copyright (c) 2015 Waldemar Walczak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VratislaviaGuide/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VratislaviaGuide
+//
+//  Created by Waldemar Walczak on 22/03/15.
+//  Copyright (c) 2015 Waldemar Walczak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
